422d619c70a4b21805bc3a527d75fa18eb0b00cf
Kiran-Sawant <kbsawant1005@gmail.com>
2021-04-10 21:22:51 +0530

    CBVs for group app

---
+++ star_social/groups/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render
+from django.views import generic as gen
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.urls import (reverse, reverse_lazy)
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.db import IntegrityError
+
+from . import models
+
+# Create your views here.
+class CreateGroup(LoginRequiredMixin, gen.CreateView):
+    """CBV for creating a Group instance."""
+
+    model = models.Group
+    fields = ('name', 'description')
+
+
+class SingleGroup(gen.DetailView):
+    """Detail view of a single Group instance."""
+
+    model = models.Group
+
+
+class ListGroup(gen.ListView):
+    """ListView for a list of all Group instances."""
+
+    model = models.Group
+
+
+class JoinGroup(LoginRequiredMixin, gen.RedirectView):
+    """Redirect view for carring out certain operations before joining a Group"""
+    
+    def get_redirect_url(self, *args, **kwargs):
+        """RedirectView method to pass a URL to redirect the user"""
+
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self, *args, **kwargs):
+        """Overriding Redirect view method.\n
+        Things to do on a get request on JoinGroup view"""
+
+        # Creating a Group instance
+        group = get_object_or_404(models.Group, slug=self.kwargs.get('slug'))
+
+        try:    # try creating a GroupMember instance from active User.
+            models.GroupMember.objects.create(user=self.request.user, group=group)
+        except IntegrityError:      # If active User is already a Member.
+            messages.warning(self.request, 'You are already a member of this group!')
+        else:
+            messages.success(self.request, 'You are now a member of this group!')
+
+        return super().get(self.request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, gen.RedirectView):
+    """RedirectView for Leaving a Group"""
+    
+    def get_redirect_url(self, *args, **kwargs):
+        """RedirectView method to pass a URL to redirect the user"""
+        
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            membership = models.GroupMember.objects.filter(
+                user = self.request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry you are not in this group!')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You have left the Group!')
+
+        return super().get(self.request, *args, **kwargs)
