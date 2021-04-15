cd701529a83704c36853dd8044157a95cb56042b
Kiran-Sawant <kbsawant1005@gmail.com>
2021-04-15 21:46:47 +0530

    added comment views and pagination to PostDetail

--- star_social/posts/views.py
+++ star_social/posts/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import (render, get_object_or_404, redirect)
+from django.urls import reverse_lazy, reverse
 from django.http import Http404
 from django.views import generic as gen
+from django.views.generic.list import MultipleObjectMixin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from braces.views import SelectRelatedMixin
@@ -46,10 +48,17 @@ class UserPosts(gen.ListView):
         return context
 
 
-class PostDetail(SelectRelatedMixin, gen.DetailView):
+class PostDetail(SelectRelatedMixin, gen.DetailView, MultipleObjectMixin):
 
     model = models.Post
     select_related = ('user', 'group')
+
+    paginate_by = 8
+
+    def get_context_data(self, **kwargs):
+        comment_list = models.Comments.objects.filter(post=self.object)
+        context = super(PostDetail, self).get_context_data(object_list=comment_list, **kwargs)
+        return context
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -91,3 +100,36 @@ class DeletePost(LoginRequiredMixin, SelectRelatedMixin, gen.DeleteView):
         return super().delete(*args, **kwargs)      # calling delete method
 
 
+#######################################################
+#           Based views for Comments                  #
+#######################################################
+@login_required
+def add_commment(request, pk):
+    """A view for adding comment to a post."""
+
+    post = get_object_or_404(models.Post, pk=pk)
+
+    if request.method == 'POST':
+        form = forms.CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post             # inserting post in coments post field
+            comment.author = request.user
+            comment.save()
+            return redirect(post, permanent=True)
+        else:
+            messages.success(request, "Invalid fields!")
+    else:
+        form = forms.CommentForm()
+
+    return render(request, 'posts/comment_form.html', {'form': form, 'post':post})
+
+@login_required
+def delete_comment(request, pk):
+    """A view to delete a comment"""
+
+    comment = get_object_or_404(models.Comments, pk=pk)
+    post = get_object_or_404(models.Post, pk=comment.post.pk)
+   
+    comment.delete()
+    return redirect(post, permanent=True)
