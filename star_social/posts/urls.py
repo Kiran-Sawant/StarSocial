c52806d311c99dc8029d5995a79ad4d8d92e9503
Kiran-Sawant <kbsawant1005@gmail.com>
2021-04-15 21:47:02 +0530

    added comment urls

--- star_social/posts/urls.py
+++ star_social/posts/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     re_path(r'^by/(?P<username>[-\w]+)/$', views.UserPosts.as_view(), name='for_user'),
     re_path(r"^by/(?P<username>[-\w]+)/(?P<pk>[\d]+)/$", views.PostDetail.as_view(), name='single'),
     re_path(r"^delete/(?P<pk>[\d]+)/$", views.DeletePost.as_view(), name='delete'),
+    # comments
+    re_path(r"^(?P<pk>[\d]+)/comment/$", views.add_commment, name='comment'),
+    re_path(r"^comment/(?P<pk>[\d]+)/remove/$", views.delete_comment, name='remove_comment'),
 ]
