333d0998a67cacf70563565844b4dcbdb38b41ca
Kiran-Sawant <kbsawant1005@gmail.com>
2021-04-17 16:16:41 +0530

    modified update & deete URLs

--- star_social/posts/urls.py
+++ star_social/posts/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('new/', views.CreatePost.as_view(), name='create'),
     re_path(r'^by/(?P<username>[-\w]+)/$', views.UserPosts.as_view(), name='for_user'),
     re_path(r"^by/(?P<username>[-\w]+)/(?P<pk>[\d]+)/$", views.PostDetail.as_view(), name='single'),
-    re_path(r"^update/(?P<pk>[\d]+)/$", views.UpdatePost.as_view(), name='post_update'),
-    re_path(r"^delete/(?P<pk>[\d]+)/$", views.DeletePost.as_view(), name='delete'),
+    re_path(r"^by/(?P<username>[-\w]+)/(?P<pk>[\d]+)/like/$", views.PostLikeToggleRedirect.as_view(), name='like-toggle'),
+    re_path(r"^by/(?P<username>[-\w]+)/(?P<pk>[\d]+)/update/$", views.UpdatePost.as_view(), name='post_update'),
+    re_path(r"^by/(?P<username>[-\w]+)/(?P<pk>[\d]+)/delete/$", views.DeletePost.as_view(), name='delete'),
     # comments
     re_path(r"^(?P<pk>[\d]+)/comment/$", views.add_commment, name='comment'),
     re_path(r"^comment/(?P<pk>[\d]+)/remove/$", views.delete_comment, name='remove_comment'),
